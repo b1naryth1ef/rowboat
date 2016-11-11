21c721a1ad988701927f7f096e893e88db00384a
Andrei <b1naryth1ef@gmail.com>
2016-11-11 05:29:52 -0600

    Add notes on admin format stuff

--- rowboat/plugins/admin.py
+++ rowboat/plugins/admin.py
@@ -2,6 +2,26 @@ from rowboat import RowboatPlugin as Plugin
 from rowboat.types.plugin import PluginConfig
 
 
+"""
+levels:
+    my_role_name: 33
+    my_user_name: 300
+
+commands:
+    prefix: '!'
+
+    overrides:
+        wowsocool:
+            disabled: true
+
+        ban:
+            rename: fukem
+
+        kick:
+            level: 30000
+"""
+
+
 class AdminConfig(PluginConfig):
     pass
 
