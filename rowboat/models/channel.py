08853c2974f7c6557658f01ac22c0491b172d646
Andrei <b1naryth1ef@gmail.com>
2016-11-30 11:36:26 -0600

    Its ok to update first_message_id on boot now, fix gitignore

--- rowboat/models/channel.py
+++ rowboat/models/channel.py
@@ -35,11 +35,13 @@ class Channel(BaseModel):
         except cls.DoesNotExist:
             new = True
             obj = cls(channel_id=channel.id)
-            obj.first_message_id = cls.generate_first_message_id(channel.id)
 
         obj.guild_id = channel.guild_id or None
         obj.name = channel.name or None
         obj.topic = channel.topic or None
+
+        if new or not obj.first_message_id:
+            obj.first_message_id = cls.generate_first_message_id(channel.id)
 
         obj.save(force_insert=new)
         return obj
