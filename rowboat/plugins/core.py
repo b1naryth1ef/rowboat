ff46715f4dff81347a9b47ddbe35e3581475f84f
Andrei <b1naryth1ef@gmail.com>
2016-11-30 12:07:08 -0600

    Add eval command

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -1,3 +1,4 @@
+import pprint
 import humanize
 import functools
 
@@ -12,6 +13,8 @@ from rowboat.sql import init_db
 from rowboat.redis import rdb
 from rowboat.models.guild import Guild
 from rowboat.plugins.modlog import Actions
+
+PY_CODE_BLOCK = '```py\n{}\n```'
 
 INFO_MESSAGE = '''\
 :information_source: Rowboat V{} - more information and detailed help can be found here:\
@@ -275,3 +278,42 @@ class CorePlugin(Plugin):
         event.msg.reply('Rowboat was started {}'.format(
             humanize.naturaltime(datetime.utcnow() - self.startup)
         ))
+
+    @Plugin.command('eval', level=-1)
+    def command_eval(self, event):
+        ctx = {
+            'bot': self.bot,
+            'client': self.bot.client,
+            'state': self.bot.client.state,
+            'event': event,
+            'msg': event.msg,
+            'guild': event.msg.guild,
+            'channel': event.msg.channel,
+            'author': event.msg.author
+        }
+
+        # Mulitline eval
+        src = event.codeblock
+        if src.count('\n'):
+            lines = filter(bool, src.split('\n'))
+            if lines[-1] and 'return' not in lines[-1]:
+                lines[-1] = 'return ' + lines[-1]
+            lines = '\n'.join('    ' + i for i in lines)
+            code = 'def f():\n{}\nx = f()'.format(lines)
+            local = {}
+
+            try:
+                exec compile(code, '<eval>', 'exec') in ctx, local
+            except Exception as e:
+                event.msg.reply(PY_CODE_BLOCK.format(type(e).__name__ + ': ' + str(e)))
+                return
+
+            event.msg.reply(PY_CODE_BLOCK.format(pprint.pformat(local['x'])))
+        else:
+            try:
+                result = eval(src, ctx)
+            except Exception as e:
+                event.msg.reply(PY_CODE_BLOCK.format(type(e).__name__ + ': ' + str(e)))
+                return
+
+            event.msg.reply(PY_CODE_BLOCK.format(result))
