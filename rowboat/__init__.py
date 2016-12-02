f1d1710ea4a040adf71373b9d11ae401fd5c8647
Andrei <b1naryth1ef@gmail.com>
2016-12-02 15:23:47 -0600

    Add sentry/raven

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -1,6 +1,10 @@
 from disco.bot import Plugin
+from raven import Client
 
 VERSION = '0.0.1'
+
+
+client = Client('https://d4e453a9eaf940ca9ddcc73c139120de:f173949164834738aaac632708dac695@sentry.io/119318')
 
 
 class RowboatPlugin(Plugin):
