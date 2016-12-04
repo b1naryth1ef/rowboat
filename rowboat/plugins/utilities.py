cd3418283ddd3cbd4eef16b1d243cdfdcd0b319b
Andrei <b1naryth1ef@gmail.com>
2016-12-03 21:19:57 -0600

    add words command

--- rowboat/plugins/utilities.py
+++ rowboat/plugins/utilities.py
@@ -8,8 +8,12 @@ from PIL import Image
 from pyquery import PyQuery
 from gevent.pool import Pool
 from datetime import datetime
-from disco.types.message import MessageEmbed, MessageEmbedField, MessageEmbedThumbnail
+from disco.types.message import MessageTable, MessageEmbed, MessageEmbedField, MessageEmbedThumbnail
 from disco.util.snowflake import to_datetime
+
+from disco.types.user import User as DiscoUser
+from disco.types.guild import Guild as DiscoGuild
+from disco.types.channel import Channel as DiscoChannel
 
 from rowboat import RowboatPlugin as Plugin
 from rowboat.types.plugin import PluginConfig
@@ -259,3 +263,37 @@ class UtilitiesPlugin(Plugin):
 
         embed.color = get_dominant_colors_user(user)
         event.msg.reply('', embed=embed)
+
+    @Plugin.command('words', '<target:user|channel|guild>')
+    def words(self, event, target):
+        if isinstance(target, DiscoUser):
+            q = 'author_id'
+        elif isinstance(target, DiscoChannel):
+            q = 'channel_id'
+        elif isinstance(target, DiscoGuild):
+            q = 'guild_id'
+        else:
+            raise Exception("You should not be here")
+
+        sql = """
+            SELECT word, count(*)
+            FROM (
+                SELECT regexp_split_to_table(content, '\s') as word
+                FROM messages
+                WHERE {}=%s
+                LIMIT 3000000
+            ) t
+            GROUP BY word
+            ORDER BY 2 DESC
+            LIMIT 30
+        """.format(q)
+
+        t = MessageTable()
+        t.set_header('Word', 'Count')
+
+        for word, count in Message.raw(sql, (target.id, )).tuples():
+            if '```' in word:
+                continue
+            t.add(word, count)
+
+        event.msg.reply(t.compile())
