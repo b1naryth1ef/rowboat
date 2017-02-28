da900a46ca14f4798688a924564cc8589800ebd9
Andrei <b1naryth1ef@gmail.com>
2017-02-28 10:50:34 -0800

    ayyy

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -1,14 +1,17 @@
 import os
 
 from raven import Client
+from raven.transport.gevent import GeventedHTTPTransport
+
 VERSION = '0.0.1'
 
-
 raven_client = Client(
-    'https://7814a66f7ec44069be50f954f969e6e6:0e51337d3c1442a0b3521422b4db3bca@sentry.hydr0.com/2',
+    'https://7814a66f7ec44069be50f954f969e6e6:0e51337d3c1442a0b3521422b4db3bca@sentry.hydr0.com/2?verify_ssl=0',
     ignore_exceptions=[
         'KeyboardInterrupt',
     ],
     release=VERSION,
-    environment=os.getenv('ENV', 'local')
+    environment=os.getenv('ENV', 'local'),
+    transport=GeventedHTTPTransport,
+
 )
