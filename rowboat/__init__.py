122f0072a15912f0e487ec9a3e12c3f5063addc4
Andrei <b1naryth1ef@gmail.com>
2017-03-21 17:30:18 -0700

    Version should be the git revision

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -1,19 +1,19 @@
 import os
+import subprocess
 
 from raven import Client
 from raven.transport.gevent import GeventedHTTPTransport
 
-VERSION = '0.0.1'
-
 ENV = os.getenv('ENV', 'local')
 DSN = os.getenv('DSN')
+REV = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
 
 raven_client = Client(
     DSN,
     ignore_exceptions=[
         'KeyboardInterrupt',
     ],
-    release=VERSION,
+    release=REV,
     environment=ENV,
     transport=GeventedHTTPTransport,
 )
