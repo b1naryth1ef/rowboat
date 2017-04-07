7b15d008de6dbbfcb9b33720d97be7f0c2ad6b48
andrei <b1naryth1ef@gmail.com>
2017-04-06 19:43:35 -0700

    backfill hype

--- rowboat/models/migrations/0009_use_arrays_idiot.py
+++ rowboat/models/migrations/0009_use_arrays_idiot.py
@@ -1,6 +1,35 @@
 from rowboat.models.migrations import Migrate
 from rowboat.models.message import Message
 
+from rowboat.sql import database as db
+from postgres_ext import ServerSide
+
+
+def backfill_column(table, old_column, new_column, apply_func=None):
+    total = table.select().count()
+
+    q = ServerSide(table.select(
+        table._meta.primary_key,
+        old_column
+    )).tuples()
+
+    idx = 0
+
+    with db.transaction() as txn:
+        for oid, old_value in q:
+            idx += 1
+            new_value = apply_func(old_value) if apply_func else old_value
+
+            if idx % 1000 == 0:
+                print 'Backfilling %s %s/%s' % (str(table), idx, total)
+                txn.commit()
+
+            table.update(
+                **{new_column: new_value}
+            ).where(table._meta.primary_key == oid).execute()
+
+    txn.commit()
+
 
 @Migrate.only_if(Migrate.missing, Message, 'mentions_new')
 def add_guild_columns(m):
@@ -11,3 +40,10 @@ def add_guild_columns(m):
         Message.attachments_new,
         Message.embeds,
     )
+
+
+@Migrate.always
+def backfill_data():
+    backfill_column(Message, Message.mentions, Message.mentions_new)
+    backfill_column(Message, Message.emojis, Message.emojis_new)
+    backfill_column(Message, Message.attachments, Message.attachments_new)
