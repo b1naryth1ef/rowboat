69c25c6efe72abbe142e4127e72be3b9297706b7
Andrei <b1naryth1ef@gmail.com>
2017-04-19 10:28:35 -0700

    eventual is fucked

--- rowboat/util/timing.py
+++ rowboat/util/timing.py
@@ -12,39 +12,29 @@ class Eventual(object):
 
     def __init__(self, func):
         self.func = func
-        self.lock = Semaphore()
-
         self._next = None
         self._t = None
 
     def wait(self, nxt):
         def f():
             gevent.sleep((self._next - datetime.utcnow()).seconds)
+            self._next = None
+            self.func()
 
-            with self.lock:
-                self._t = None
-                self._next = None
-                self.func()
+        if self._t:
+            self._t.kill()
 
-        with self.lock:
-            if self._t:
-                self._t.kill()
-                self._t = None
-
-            self._next = nxt
-            self._t = gevent.spawn(f)
+        self._next = nxt
+        self._t = gevent.spawn(f)
 
     def trigger(self):
-        with self.lock:
-            if self._t:
-                self._t.kill()
-                self._t = None
-                self._next = None
-            self.func()
+        if self._t:
+            self._t.kill()
+        self._next = None
+        self.func()
 
     def set_next_schedule(self, date):
         if date < datetime.utcnow():
-            self._next = None
             return gevent.spawn(self.trigger)
 
         if not self._next or date < self._next:
