c50dba0bd9c8ae748ac4c398a9e8f0ba7ad398cb
Andrei <b1naryth1ef@gmail.com>
2017-05-08 18:49:16 -0700

    Add command exception logic

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -5,6 +5,7 @@ import signal
 import inspect
 import humanize
 import functools
+import traceback
 import contextlib
 
 from datetime import datetime, timedelta
@@ -365,6 +366,31 @@ class CorePlugin(Plugin):
                 command.plugin.execute(CommandEvent(command, event.message, match))
             except CommandResponse as e:
                 return event.reply(e.response)
+            except:
+                embed = MessageEmbed()
+                embed.color = 0xb19cd9
+                embed.title = 'Command Error'
+                embed.description = '```\n{}\n```'.format(
+                    traceback.format_exc()
+                )
+                embed.add_field(name='Command', value=command.name, inline=True)
+                embed.add_field(name='Plugin', value=command.plugin.name, inline=True)
+                embed.add_field(name='Author', value=unicode(event.author), inline=True)
+                if event.guild:
+                    embed.add_field(name='Guild', value=u'{} (`{}`)'.format(
+                        event.guild.name,
+                        event.guild.id,
+                    ), inline=True)
+
+                embed.add_field(name='Channel', value=u'{} (`{}`)'.format(
+                    unicode(event.channel.name),
+                    event.channel.id,
+                ), inline=True)
+
+                if ENV != 'prod':
+                    self.bot.client.api.channels_messages_create(311317125178327042, embed=embed)
+
+                return event.msg.reply('<:{}> something went wrong, perhaps try again later'.format(RED_TICK_EMOJI))
 
             Message.update(command=command.plugin.name + ':' + command.name).where(
                 (Message.id == event.message.id)
