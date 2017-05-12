6edecda8a5b720b3181d982b5b1b116b45c76068
Andrei <b1naryth1ef@gmail.com>
2017-05-11 17:47:56 -0700

    Fix timed tags

--- rowboat/util/stats.py
+++ rowboat/util/stats.py
@@ -5,11 +5,11 @@ from datadog import statsd
 
 
 @contextmanager
-def timed(metricname, **tags):
+def timed(metricname, tags=None):
     start = time.time()
     try:
         yield
     except:
         raise
     finally:
-        statsd.timing(metricname, (time.time() - start) * 1000, tags=['{}:{}'.format(k, v) for k, v in tags.items()])
+        statsd.timing(metricname, (time.time() - start) * 1000, tags=['{}:{}'.format(k, v) for k, v in (tags or {}).items()])
