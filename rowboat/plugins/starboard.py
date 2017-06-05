6a9120728c164d42c22e40acad800cda1e900d92
Andrei <b1naryth1ef@gmail.com>
2017-06-05 15:49:16 -0700

    [starboard] add show command

--- rowboat/plugins/starboard.py
+++ rowboat/plugins/starboard.py
@@ -7,7 +7,7 @@ from disco.bot import CommandLevels
 from disco.api.http import APIException
 from disco.types.message import MessageEmbed
 
-from rowboat.plugins import RowboatPlugin as Plugin
+from rowboat.plugins import RowboatPlugin as Plugin, CommandFail
 from rowboat.types.plugin import PluginConfig
 from rowboat.types import ChannelField, Field, SlottedModel, ListField, DictField
 from rowboat.models.user import StarboardBlock, User
@@ -71,6 +71,32 @@ class StarboardPlugin(Plugin):
         super(StarboardPlugin, self).load(ctx)
         self.updates = {}
         self.locks = {}
+
+    @Plugin.command('show', '<mid:snowflake>', group='stars', level=CommandLevels.TRUSTED)
+    def stars_show(self, event, mid):
+        try:
+            star = StarboardEntry.select().join(Message).where(
+                (Message.guild_id == event.guild.id) &
+                (~(StarboardEntry.star_message_id >> None)) &
+                (
+                    (Message.id == mid) |
+                    (StarboardEntry.star_message_id == mid)
+                )
+            ).get()
+        except StarboardEntry.DoesNotExist:
+            raise CommandFail('no starboard message with that id')
+
+        _, sb_config = event.config.get_board(star.message.channel_id)
+
+        try:
+            source_msg = self.client.api.channels_messages_get(
+                star.message.channel_id,
+                star.message_id)
+        except:
+            raise CommandFail('no starboard message with that id')
+
+        content, embed = self.get_embed(star, source_msg, sb_config)
+        event.msg.reply(content, embed=embed)
 
     @Plugin.command('stats', '[user:user]', group='stars', level=CommandLevels.MOD)
     def stars_stats(self, event, user=None):
