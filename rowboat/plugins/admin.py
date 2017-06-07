047aec630db5fbc22648286a1a77166ad0f1bf37
andrei <b1naryth1ef@gmail.com>
2017-06-06 17:18:44 -0700

    [admin] add mkick command

--- rowboat/plugins/admin.py
+++ rowboat/plugins/admin.py
@@ -453,14 +453,20 @@ class AdminPlugin(Plugin):
         else:
             raise CommandFail('I couldn\t find any member backups for that user')
 
-    def can_act_on(self, event, victim_id):
+    def can_act_on(self, event, victim_id, throw=True):
         if event.author.id == victim_id:
+            if not throw:
+                return False
             raise CommandFail('cannot execute that action on yourself')
 
         victim_level = self.bot.plugins.get('CorePlugin').get_level(event.guild, victim_id)
 
         if event.user_level <= victim_level:
+            if not throw:
+                return False
             raise CommandFail('invalid permissions')
+
+        return True
 
     @Plugin.command('mute', '<user:user|snowflake> [reason:str...]', level=CommandLevels.MOD)
     def mute(self, event, user, reason=None):
@@ -564,6 +570,48 @@ class AdminPlugin(Plugin):
                 ))
         else:
             raise CommandFail('invalid user')
+
+    @Plugin.command('mkick', parser=True, level=CommandLevels.MOD)
+    @Plugin.parser.add_argument('users', type=long, nargs='+')
+    @Plugin.parser.add_argument('-r', '--reason', default='', help='reason for modlog')
+    def mkick(self, event, args):
+        members = []
+        for user_id in args.users:
+            member = event.guild.get_member(user_id)
+            if not member:
+                # TODO: this sucks, batch these
+                raise CommandFail('failed to kick {}, user not found'.format(user_id))
+
+            if not self.can_act_on(event, member.id, throw=False):
+                raise CommandFail('failed to kick {}, invalid permissions'.format(user_id))
+
+            members.append(member)
+
+        msg = event.msg.reply('Ok, kick {} users for `{}`?'.format(len(members), args.reason or 'no reason'))
+        msg.chain(False).\
+            add_reaction(GREEN_TICK_EMOJI).\
+            add_reaction(RED_TICK_EMOJI)
+
+        try:
+            mra_event = self.wait_for_event(
+                'MessageReactionAdd',
+                message_id=msg.id,
+                conditional=lambda e: (
+                    e.emoji.id in (GREEN_TICK_EMOJI_ID, RED_TICK_EMOJI_ID) and
+                    e.user_id == event.author.id
+                )).get(timeout=10)
+        except gevent.Timeout:
+            return
+        finally:
+            msg.delete()
+
+        if mra_event.emoji.id != GREEN_TICK_EMOJI_ID:
+            return
+
+        for member in members:
+            Infraction.kick(self, event, member, args.reason)
+
+        raise CommandSuccess('kicked {} users'.format(len(members)))
 
     @Plugin.command('ban', '<user:user|snowflake> [reason:str...]', level=CommandLevels.MOD)
     @Plugin.command('forceban', '<user:snowflake> [reason:str...]', level=CommandLevels.MOD)
@@ -692,7 +740,7 @@ class AdminPlugin(Plugin):
                 add_reaction(RED_TICK_EMOJI)
 
             try:
-                event = self.wait_for_event(
+                mra_event = self.wait_for_event(
                     'MessageReactionAdd',
                     message_id=msg.id,
                     conditional=lambda e: (
@@ -704,7 +752,7 @@ class AdminPlugin(Plugin):
             finally:
                 msg.delete()
 
-            if event.emoji.id != GREEN_TICK_EMOJI_ID:
+            if mra_event.emoji.id != GREEN_TICK_EMOJI_ID:
                 return
 
             event.msg.reply(':wastebasket: Ok please hold on while I delete those messages...').after(5).delete()
@@ -893,7 +941,7 @@ class AdminPlugin(Plugin):
             add_reaction(RED_TICK_EMOJI)
 
         try:
-            event = self.wait_for_event(
+            mra_event = self.wait_for_event(
                 'MessageReactionAdd',
                 message_id=msg.id,
                 conditional=lambda e: (
@@ -907,7 +955,7 @@ class AdminPlugin(Plugin):
 
         msg.delete()
 
-        if event.emoji.id == GREEN_TICK_EMOJI_ID:
+        if mra_event.emoji.id == GREEN_TICK_EMOJI_ID:
             msg = msg.reply('Pruning invites...')
             for invite in invites:
                 invite.delete()
