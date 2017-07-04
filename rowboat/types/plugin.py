65bfec405a17a58a98d6661a9001be4620dbd3da
andrei <b1naryth1ef@gmail.com>
2017-07-03 23:40:07 -0700

    [etc] invert check

--- rowboat/types/plugin.py
+++ rowboat/types/plugin.py
@@ -7,6 +7,6 @@ class PluginConfig(SlottedModel):
     def load(self, obj, *args, **kwargs):
         obj = {
             k: v for k, v in six.iteritems(obj)
-            if k in self._fields and self._fields[k].metadata.get('private')
+            if k in self._fields and not self._fields[k].metadata.get('private')
         }
         return super(PluginConfig, self).load(obj, *args, **kwargs)
